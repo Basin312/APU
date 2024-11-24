30f4b9b01ce36c594b38ac241f12c9a88734bb62
samuelharatua <samuelharatua0117@gmail.com>
2024-11-25 06:28:56 +0700

    menambahkan top produk dan top umk

--- src/main/java/com/example/demo/admin/AdminControlling.java
+++ src/main/java/com/example/demo/admin/AdminControlling.java
@@ -2,14 +2,31 @@ package com.example.demo.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminControlling {
      
-    @GetMapping("")
-    public String halamanLogin(){
-        return "umkOverview";
+    @GetMapping("/admin")
+    public String showAdmin(){
+        return "admin/topProduk";
+    }
+    @GetMapping("/topProduk")
+    public String showTopProduk(){
+        return "admin/topProduk";
+    }
+
+    @GetMapping("/umkOverview")
+    public String showUmkOverview() {
+        return "admin/umkOverview";
+    }
+
+    @GetMapping("/index")
+    public String showindex() {
+        return "index";
+    }
+
+    @GetMapping("/topUmk")
+    public String showTopUmk() {
+        return "admin/TopUMK";
     }
 }
